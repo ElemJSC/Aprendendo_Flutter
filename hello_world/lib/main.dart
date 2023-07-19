d2260529bcfff1dd863492a04485ef3d4b785771
ElemJSC <117487696+ElemJSC@users.noreply.github.com>
2023-07-19 07:24:27 -0300

    Hello Word
    Primeiro código

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Meu Primeiro App Flutter'),
+      ),
+      body: Center(
+        child: Text('Olá, Mundo!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ),
+  ));
+}
+
+
+ 
+
+ 
